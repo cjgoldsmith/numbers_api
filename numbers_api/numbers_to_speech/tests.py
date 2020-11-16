213caa94a1b5da24a717a7f2572cf063f2fe7f16
CJ Goldsmith <carlos.j.goldsmith@gmail.com>
2020-11-16 07:41:41 -0800

    Adding the ability to parse arbitrary length number strings.

--- numbers_api/numbers_to_speech/tests.py
+++ numbers_api/numbers_to_speech/tests.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from numbers_to_speech.views import ConvertView
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -10,7 +11,16 @@ class ConvertViewTests(APITestCase):
 
     def test_one_hundred(self):
 
-        url = reverse('ntos:convert', kwargs={'number': 100})
+        url = reverse('ntos:convert', kwargs={'number': 123})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json().get('num_in_english'), 'one hundo')
+        self.assertEqual(response.json().get('num_in_english'), 'one hundred twenty three')
+
+    def test_parse_hundred(self):
+        self.assertEqual(ConvertView.parse_hundred('023'), 'twenty three')
+        self.assertEqual(ConvertView.parse_hundred('005'), 'five')
+        self.assertEqual(ConvertView.parse_hundred('955'), 'nine hundred fifty five')
+
+    def test_parse_number(self):
+        self.assertEqual(ConvertView.parse_number('1024'), 'one thousand twenty four')
+        self.assertEqual(ConvertView.parse_number('2541024'), 'two million five hundred fourty one thousand twenty four')
--- numbers_api/numbers_to_speech/views.py
+++ numbers_api/numbers_to_speech/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from textwrap import wrap
+import math
 
 
 class ConvertView(APIView):
@@ -8,12 +10,86 @@ class ConvertView(APIView):
     Convert an integer based string to an enlish
     readable snippet representing that integer.
     """
+    ONES = {
+        '0': '',
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine',
+    }
+
+    TENS = {
+        '0': '',
+        '1': 'ten',
+        '2': 'twenty',
+        '3': 'thirty',
+        '4': 'fourty',
+        '5': 'fifty',
+        '6': 'sixty',
+        '7': 'seventy',
+        '8': 'eighty',
+        '9': 'ninety',
+    }
+
+    # 10^{key * 3}
+    MAGNITUDE = {
+        0: '',
+        1: 'thousand',
+        2: 'million',
+        3: 'billion',
+        4: 'trillion',
+        5: 'quadrillion',
+        6: 'quintillion',
+        7: 'sextillion',
+        8: 'septillion',
+        9: 'octillion',
+        10: 'nonillion',
+        11: 'decillion',
+        12: 'undecillion',
+        13: 'duodecillion',
+        14: 'tredecillion',
+    }
 
     def get(self, request, number, format=None):
-        breakpoint()
         return Response(
             {
                 'status': 'ok',
-                'num_in_english': 'one hundo',
+                'num_in_english': self.parse_hundred(number),
             }, status=status.HTTP_200_OK,
         )
+
+    @classmethod
+    def parse_number(cls, number_string):
+        parsed = ''
+
+        # We require full hundreds sets in each magnitude.
+        padded_length = math.ceil(len(number_string)/3) * 3
+        number_string = number_string.zfill(padded_length)
+
+        number_by_magnitude = wrap(number_string, 3)
+        idx = len(number_by_magnitude) - 1
+        for hundreds_string in number_by_magnitude:
+            parsed += f'{cls.parse_hundred(hundreds_string)} {cls.MAGNITUDE[idx]}'.strip()
+            parsed += ' '
+            idx -= 1
+        return parsed.rstrip()
+
+
+    @classmethod
+    def parse_hundred(cls, hundreds_string):
+        # programming error, do not continue.
+        if len(hundreds_string) != 3:
+            raise ValueError("parse_hundred called with less than hundreds value string")
+
+        hundreds = cls.ONES[hundreds_string[0]]
+        if hundreds:
+            hundreds = f'{hundreds} hundred'
+
+        tens = cls.TENS[hundreds_string[1]]
+        ones = cls.ONES[hundreds_string[2]]
+        return ' '.join([hundreds, tens, ones]).strip()
